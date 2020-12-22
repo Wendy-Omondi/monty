25d0fa96bf229fa7656689273ec0267f52dd4421
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-12-22 14:25:22 +0000

    main

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+ *main - Entry point
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: integer
+ */
+int main(int argc, char **argv)
+{
+if (argc != 2)
+arguments();
+
+open(argv);
+return (0);
+}
