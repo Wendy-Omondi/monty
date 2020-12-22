944e1fa3af6c602558a95b79bd3dfc0673ae74d1
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-12-22 17:31:41 +0000

    betty fix

--- push_pall_pint.c
+++ push_pall_pint.c
@@ -58,15 +58,17 @@ void free_it(stack_t *stack)
 stack_t *temp;
 
 if (stack == NULL)
-return (0);
+return;
 
 while (stack != NULL)
 {
 temp = stack;
 stack = stack->next;
+free(temp);
+}
 free(stack);
 }
-}
+
 /**
  *pint - prints value at the top of the line
  *@stack: double pointer to head (top of stack)
@@ -85,4 +87,25 @@ exit(EXIT_FAILURE);
 node = *stack;
 (*stack) = (*stack)->next;
 free(node);
+}
+
+/**
+ *pop - prints value at the top of the line
+ *@stack: double pointer to head (top of stack)
+ *@line_number: value of new node
+ *Return: Void
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+stack_t *temp;
+temp = *stack;
+(void) line_number;
+if (stack == NULL)
+{
+fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+exit(EXIT_FAILURE);
+}
+temp = temp->next;
+free(*stack);
+*stack = temp;
 }
