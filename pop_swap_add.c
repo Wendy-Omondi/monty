d543399450bf4819a7c60c0210f39f41b1b6814d
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-12-22 17:32:15 +0000

    betty fix

--- pop_swap_add.c
+++ pop_swap_add.c
@@ -1,27 +1,6 @@
 #include "monty.h"
 
 /**
- *pop - prints value at the top of the line
- *@stack: double pointer to head (top of stack)
- *@line_number: value of new node
- *Return: Void
- */
-void pop(stack_t **stack, unsigned int line_number)
-{
-stack_t *temp;
-temp = *stack;
-(void) line_number;
-if (stack == NULL)
-{
-fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
-exit(EXIT_FAILURE);
-}
-temp = temp->next;
-free(*stack);
-*stack = temp;
-}
-
-/**
  *swap_it - swaps the top two elements of a stack
  *@stack: double pointer to head (top of stack)
  *@line_number: value of new node
