0ea3b5b889e3aea8b09664883457347ed19a4e05
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-12-22 18:03:57 +0000

    more opcodes

--- pop_swap_add.c
+++ pop_swap_add.c
@@ -40,3 +40,68 @@ temp = (*stack)->next;
 temp->n += (*stack)->n;
 pop(stack, line_number);
 }
+
+/**
+ *sub_it -main entry.
+ *Description:Sub the top element of stck from the 2d top elem of the stak
+ * @stack: element at the top of the stack (head)
+ * @line_number: constant int value in the structure
+ * Return: void
+ **/
+void sub_it(stack_t **stack, unsigned int line_number)
+{
+stack_t *temp;
+
+if (*stack == NULL || (*stack)->next == NULL)
+fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+exit(EXIT_FAILURE);
+
+temp = (*stack)->next;
+temp->n -= (*stack)->n;
+pop(stack, line_number);
+}
+
+/**
+ * div_it - divides second top element of stack by top element of the stack.
+ * @stack: double pointer to head (top of stack)
+ * @line_number: value of element
+ * Return: void
+ */
+
+void div_it(stack_t **stack, unsigned int line_number)
+{
+stack_t *temp;
+
+if (*stack == NULL || (*stack)->next == NULL)
+
+fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+exit(EXIT_FAILURE);
+
+if ((*stack)->n == 0)
+fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+exit(EXIT_FAILURE);
+
+temp = (*stack)->next;
+temp->n = (temp->n) / (*stack)->n;
+pop(stack, line_number);
+}
+/**
+ * mul_it - multiplies second top element of stack with top element of stack.
+ * @stack: double pointer to head
+ * @line_number: value of element
+ * Return: void
+ */
+void mul_it(stack_t **stack, unsigned int line_number)
+{
+stack_t *temp;
+
+if (*stack == NULL || (*stack)->next == NULL)
+{
+fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
+exit(EXIT_FAILURE);
+}
+
+temp = (*stack)->next;
+temp->n *= (*stack)->n;
+pop(stack, line_number);
+}
