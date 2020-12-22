05ffb598813daf2c28abc8dbcda1dc3951eefa2d
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-12-22 14:22:45 +0000

    more opcodes

--- push_pall.c
+++ push_pall.c
@@ -60,7 +60,7 @@ stack_t *node;
 (void) line_number;
 if (stack == NULL || *stack == NULL)
 {
-printf("L%u: can't pint, stack empty\n", line_number);
+fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 exit(EXIT_FAILURE);
 }
 node = *stack;
@@ -81,10 +81,52 @@ temp = *stack;
 (void) line_number;
 if (stack == NULL)
 {
-printf("L%u: can't pop an empty stack\n", line_number);
+fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 exit(EXIT_FAILURE);
 }
 temp = temp->next;
 free(*stack);
 *stack = temp;
+}
+
+/**
+ *swap - swaps the top two elements of a stack
+ *@stack - double pointer to head (top of stack)
+ *@line_number: value of new node
+ *Return: Void
+ */
+void swap_it(stack_t **stack, unsigned int line_number)
+{
+int i;
+
+if(*stack == NULL || (*stack)->next == NULL)
+{
+fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+exit(EXIT_FAILURE);
+}
+
+i = (*stack)->n;
+(*stack)->n = (*stack)->next->n;
+(*stack)->next->n = i;
+}
+
+/**
+ *add - adds the top two elements of the stack
+ *@stack: double pointer to head (top of stack)
+ *@line_number: value of new node
+ *Return: void
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+stack_t *temp;
+
+if(*stack == NULL || (*stack)->next == NULL)
+{
+fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+exit(EXIT_FAILURE);
+}
+
+temp = (*stack)->next;
+temp->n += (*stack)->n;
+pop(stack, line_number);
 }
