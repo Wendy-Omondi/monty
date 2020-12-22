678af5d454c7e6223fb87c3526c8b04cb2684f28
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-12-22 13:51:33 +0000

    opcodes

--- push_pall.c
+++ push_pall.c
@@ -47,3 +47,44 @@ printf("%d\n", temp->n);
 temp = temp->next;
 }
 }
+
+/**
+ *pint - prints value at the top of the line
+ *@stack: double pointer to head (top of stack)
+ *@line_number: value of new node
+ *Return: Void
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+stack_t *node;
+(void) line_number;
+if (stack == NULL || *stack == NULL)
+{
+printf("L%u: can't pint, stack empty\n", line_number);
+exit(EXIT_FAILURE);
+}
+node = *stack;
+(*stack) = (*stack)->next;
+free(node);
+}
+
+/**
+ *pop - prints value at the top of the line
+ *@stack: double pointer to head (top of stack)
+ *@line_number: value of new node
+ *Return: Void
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+stack_t *temp;
+temp = *stack;
+(void) line_number;
+if (stack == NULL)
+{
+printf("L%u: can't pop an empty stack\n", line_number);
+exit(EXIT_FAILURE);
+}
+temp = temp->next;
+free(*stack);
+*stack = temp;
+}
